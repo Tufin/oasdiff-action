4f78578064deb7aac50f82c596bef8347f240586
Reuven <rh@tufin.com>
2022-03-30 12:34:11 +0300

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh -l
 
-path=$(path)
-oasdiff -format text -base /github/workspace/$path -revision /github/workspace/$path
+echo "Hello $1"
+oasdiff -format text -base /github/workspace/$(path) -revision /github/workspace/$(path)
