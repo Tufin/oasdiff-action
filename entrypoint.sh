04998702f3cd1a352ead826aad45867076f9cecf
effi <effoeffi@gmail.com>
2022-06-30 16:10:00 +0300

    fail on exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-oasdiff -format "$3" -base "$4" -revision "$5" -breaking-only true -fail-on-diff true
+oasdiff -format "$3" -base "$4" -revision "$5" -breaking-only -fail-on-diff
