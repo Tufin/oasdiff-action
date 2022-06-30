38b15288a27a3b42d589976de74fce47399a477f
effi <effoeffi@gmail.com>
2022-06-30 16:44:33 +0300

    fail on exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,15 +7,15 @@ readonly format="$3"
 readonly breaking_only="$4"
 readonly fail_on_diff="$5"
 
-if [ "$breaking_only" ] && [ "$fail_on_diff" ]
+if [ "$breaking_only" = "true" ] && [ "$fail_on_diff" = "true"  ]
 then
   echo "running breaking_only and fail_on_diff..."
   oasdiff -breaking-only -fail-on-diff -format "$format" -base "$base" -revision "$revision"
-elif [ "$breaking_only" ]
+elif [ "$breaking_only" = "true" ]
 then
   echo "running breaking_only..."
   oasdiff -breaking-only -format "$format" -base "$base" -revision "$revision"
-elif [ "$fail_on_diff" ]
+elif [ "$fail_on_diff" = "true" ]
 then
   echo "running fail_on_diff..."
   oasdiff -fail-on-diff -format "$format" -base "$base" -revision "$revision"
