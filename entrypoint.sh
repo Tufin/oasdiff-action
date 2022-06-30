230122b40f3e11e7e73306c031b674af7e09da90
effi <effoeffi@gmail.com>
2022-06-30 16:04:18 +0300

    fail on exit code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-set -e
+#!/bin/sh -l
+
+echo "1 $1"
+echo "2 $2"
+echo "3 $3"
+echo "4 $4"
+echo "5 $5"
 oasdiff -breaking-only "$1" -fail-on-diff "$2" -format "$3" -base "$4" -revision "$5"
