29f59216a28a6ad177a83f3f4f1b34be2fd070ba
Effi <effoeffi@users.noreply.github.com>
2022-06-30 20:25:30 +0300

    Breaking changes added (#2)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,25 @@
-#!/bin/sh -l
+#!/bin/sh
+set -e
 
-oasdiff -format "$1" -base "$2" -revision "$3"
+readonly base="$1"
+readonly revision="$2"
+readonly format="$3"
+readonly breaking_only="$4"
+readonly fail_on_diff="$5"
+
+if [ "$breaking_only" = "true" ] && [ "$fail_on_diff" = "true"  ]
+then
+  echo "running breaking_only and fail_on_diff..."
+  oasdiff -breaking-only -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+elif [ "$breaking_only" = "true" ]
+then
+  echo "running breaking_only..."
+  oasdiff -breaking-only -format "$format" -base "$base" -revision "$revision"
+elif [ "$fail_on_diff" = "true" ]
+then
+  echo "running fail_on_diff..."
+  oasdiff -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+else
+  echo "running default..."
+  oasdiff -format "$format" -base "$base" -revision "$revision"
+fi
