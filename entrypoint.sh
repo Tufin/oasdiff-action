1ffea11402dda4c52fc25bc9e07f7e1184569c76
effoeffi <effoeffi@gmail.com>
2023-04-03 22:12:08 +0300

    support check breaking

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,8 +6,17 @@ readonly revision="$2"
 readonly format="$3"
 readonly breaking_only="$4"
 readonly fail_on_diff="$5"
+readonly check_breaking="$6"
 
-if [ "$breaking_only" = "true" ] && [ "$fail_on_diff" = "true"  ]
+if [ "$check_breaking" = "true" ] && [ "$fail_on_diff" = "true"  ]
+then
+  echo "running check_breaking and fail_on_diff..."
+  oasdiff -check-breaking -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+elif [ "$check_breaking" = "true" ]
+then
+  echo "running check_breaking..."
+  oasdiff -check-breaking -format "$format" -base "$base" -revision "$revision"
+elif [ "$breaking_only" = "true" ] && [ "$fail_on_diff" = "true"  ]
 then
   echo "running breaking_only and fail_on_diff..."
   oasdiff -breaking-only -fail-on-diff -format "$format" -base "$base" -revision "$revision"
