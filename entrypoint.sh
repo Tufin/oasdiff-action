4eabee556168da7d223487c7d639a67750961595
Effi <effoeffi@users.noreply.github.com>
2023-05-05 17:47:38 +0300

    remove  -no-lint

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,24 +13,24 @@ echo "running oasdiff... base: $base, revision: $revision, format: $format, brea
 if [ "$check_breaking" = "true" ] && [ "$fail_on_diff" = "true"  ]
 then
   echo "running check_breaking and fail_on_diff..."
-  oasdiff -no-lint -check-breaking -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+  oasdiff -check-breaking -fail-on-diff -format "$format" -base "$base" -revision "$revision"
 elif [ "$check_breaking" = "true" ]
 then
   echo "running check_breaking..."
-  oasdiff -no-lint -check-breaking -format "$format" -base "$base" -revision "$revision"
+  oasdiff -check-breaking -format "$format" -base "$base" -revision "$revision"
 elif [ "$breaking_only" = "true" ] && [ "$fail_on_diff" = "true"  ]
 then
   echo "running breaking_only and fail_on_diff..."
-  oasdiff -no-lint -breaking-only -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+  oasdiff -breaking-only -fail-on-diff -format "$format" -base "$base" -revision "$revision"
 elif [ "$breaking_only" = "true" ]
 then
   echo "running breaking_only..."
-  oasdiff -no-lint -breaking-only -format "$format" -base "$base" -revision "$revision"
+  oasdiff -breaking-only -format "$format" -base "$base" -revision "$revision"
 elif [ "$fail_on_diff" = "true" ]
 then
   echo "running fail_on_diff..."
-  oasdiff -no-lint -fail-on-diff -format "$format" -base "$base" -revision "$revision"
+  oasdiff -fail-on-diff -format "$format" -base "$base" -revision "$revision"
 else
   echo "running default..."
-  oasdiff -no-lint -format "$format" -base "$base" -revision "$revision"
+  oasdiff -format "$format" -base "$base" -revision "$revision"
 fi
